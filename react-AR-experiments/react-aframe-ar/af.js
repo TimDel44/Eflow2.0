b972124bfba2041c3b815daa6234257a2499e9fa
TimDel <tim.del44210@gmail.com>
2020-09-10 15:28:28 +0200

    fin de l'ath

--- react-AR-experiments/react-aframe-ar/af.js
+++ react-AR-experiments/react-aframe-ar/af.js
@@ -9,7 +9,7 @@ class AF extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: null,
+            data:{},
         }
     }
     fetchData() {
@@ -40,23 +40,35 @@ class AF extends React.Component {
     render() {
 
 
+
             return (
+
+
+
                 <div>
+
                 <a-scene embedded artoolkit='sourceType: webcam; sourceWidth: 240; sourceHeight: 180;' render="antialias: true" arjs="debugUIEnabled: false;" vr-mode-ui="enabled: false;">
                     <a-assets>
                         <a-asset-item id="optimerBoldFont" src="https://rawgit.com/mrdoob/three.js/dev/examples/fonts/optimer_bold.typeface.json"></a-asset-item>
                         <a-mixin id="boldFont" text="font: #optimerBoldFont"></a-mixin>
                     </a-assets>
                     <a-marker preset="hiro">
-                        <a-entity mixin="boldFont" text-geometry="value: What's up"></a-entity>
-                        <a-entity text-geometry="value: Dog?; font: #optimerBoldFont"></a-entity>
-                        <a-text value="Wh" rotation="-90 0 0" position="0.3 0 0" z-offset="1" anchor="align">
-                        </a-text>
-                        <a-text value={this.state.data} rotation="-90 0 0" position="0 0 0" z-offset="1" anchor="align" material="color: red; src: #optimerBoldFont">
-                        </a-text>
-                        <a-text value="Consommation :" rotation="-90 0 0" position="-1.7 0 0" z-offset="1" anchor="align">
-                        </a-text>
-                        {/*<a-text value="haut" rotation="-90 0 0" position="0 1 0" z-offset="1" anchor="align">*/}
+
+                        <a-entity mixin="boldFont" text-geometry={this.state.data.ID} rotation="-90 0 0" position="-2 0 1" z-offset="1" anchor="align" material="color: lightblue"></a-entity>
+                        <a-entity mixin="boldFont" text-geometry={this.state.data.etat} rotation="-90 0 0" position="-2 0 -1" z-offset="1" anchor="align" material="color: green"></a-entity>
+                        <a-entity mixin="boldFont" text-geometry={this.state.data.Wh} rotation="-90 0 0" position="1 0 0" z-offset="1" anchor="align" material="color: lightblue"></a-entity>
+                        <a-entity mixin="boldFont" text-geometry={this.state.data.consommation} rotation="-90 0 0" position="-2.5 0 0" z-offset="1" anchor="align" material="color: lightblue"></a-entity>
+                        <a-entity mixin="boldFont" text-geometry={this.state.data.nombre} rotation="-90 0 0" position="0 0 0" z-offset="1" anchor="align" material="color: lightblue"></a-entity>
+
+                        {/*<a-entity text-geometry="value: Dog?; font: #optimerBoldFont"></a-entity>*/}
+
+                        {/*<a-text value="Wh" rotation="-90 0 0" position="0.3 0 0" z-offset="1" anchor="align">*/}
+                        {/*</a-text>*/}
+                        {/*<a-text value={this.state.data.nombre} rotation="-90 0 0" position="0 0 0" z-offset="1" anchor="align">*/}
+                        {/*</a-text>*/}
+                        {/*<a-text value="Consommation :" rotation="-90 0 0" position="-1.7 0 0" z-offset="1" anchor="align">*/}
+                        {/*</a-text>*/}
+                        {/*<a-text value={this.state.data.nom} rotation="-90 0 0" position="0 1 0" z-offset="1" anchor="align">*/}
                         {/*</a-text>*/}
                         {/*<a-text value="bas" rotation="-90 0 0" position="0 -1 0" z-offset="1" anchor="align">*/}
                         {/*</a-text>*/}
@@ -88,4 +100,3 @@ class AF extends React.Component {
 
 const domContainer = document.querySelector('#marker');
 ReactDOM.render(e(AF), domContainer);
-
