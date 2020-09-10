ecda9cfdf1e756808908e95f812ab42bb08e7326
yohann <yohann.durel@gmail.com>
2020-09-10 11:22:33 +0200

    suppr commentaires inutiles

--- react-AR-experiments/react-aframe-ar/af.js
+++ react-AR-experiments/react-aframe-ar/af.js
@@ -45,13 +45,6 @@ class AF extends React.Component {
                 <a-scene embedded artoolkit='sourceType: webcam; sourceWidth: 240; sourceHeight: 180;' render="antialias: true" outline>
                     <a-marker preset="hiro">
 
-                        {/*<a-entity*/}
-                        {/*    position="1 0.5 0"*/}
-                        {/*    rotation="-90 0 0"*/}
-                        {/*    geometry="primitive: plane; width: 4; height: auto"*/}
-                        {/*    material="visible: false"*/}
-                        {/*    text="value: Ceci est un texte.">*/}
-                        {/*</a-entity>*/}
                         <a-text value="droite" rotation="-90 0 0" position="1 0 0" z-offset="1" anchor="align">
                         </a-text>
                         <a-text value={this.state.data} rotation="-90 0 0" position="0 0 0" z-offset="1" anchor="align">
@@ -74,12 +67,7 @@ class AF extends React.Component {
                         </a-text>
                         <a-text value="*" rotation="-90 0 0" position="-1 0 1" z-offset="1" anchor="align">
                         </a-text>
-                        {/*<a-entity  position="1 0.5 0"*/}
-                        {/*           rotation="-90 0 0"*/}
-                        {/*           geometry="primitive:plane"*/}
-                        {/*           material="shader: ocean; color: blue; wave-height: 10">*/}
 
-                        {/*</a-entity>*/}
                     </a-marker>
                     <a-entity camera></a-entity>
                 </a-scene>
